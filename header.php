74121e2f8a566847cec545ebdd9074959f3495f5
Lauren <krush.art@gmail.com>
2022-10-06 18:54:31 -0400

    Update header.php
    fix broken img links

--- header.php
+++ header.php
@@ -18,7 +18,7 @@
         <meta name="description" content="Demo Theme" />
         <meta name="author" content="Lauren Bridges" />
         <!-- Favicon-->
-        <link rel="icon" type="image/x-icon" href="wp-content/themes/xbridge/img/favicon.ico" />
+        <link rel="icon" type="image/x-icon" href="<?php echo get_template_directory_uri() . '/img/favicon.ico'; ?>" />
         <?php
 		wp_head();
 		?>
@@ -28,8 +28,8 @@
             
             <nav class="navbar navbar-expand-md fixed-top navbar-light bg-light">
                 <div class="container">
-                    <a class="navbar-brand" href="https://github.com/Lbridges/xbridge2">
-                        <img src="wp-content/themes/xbridge/img/luxury-logo.png" alt="Luxury Logo" />
+                    <a class="navbar-brand" href="https://github.com/Lbridges/xbridge/tree/master/xbridge">
+                        <img src="<?php echo get_template_directory_uri() . '/img/luxury-logo.png'; ?>" alt="Luxury Logo" />
                     </a>                
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
